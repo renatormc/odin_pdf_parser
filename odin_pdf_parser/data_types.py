b4802010e182bbe7ee196e61d3e7a5b9c76cab4b
renatormc <renatomartinsrmc@gmail.com>
2022-06-04 18:33:15 -0300

    Firt commit

---
+++ odin_pdf_parser/data_types.py
@@ -0,0 +1,26 @@
+
+class OdinParserDataPericia:
+    def __init__(self) -> None:
+        self.seq: int = 0
+        self.rg: int = 0
+        self.ano: int = 0
+
+class OdinParserDataQusito:
+    def __init__(self) -> None:
+        self.numero: str = ""
+        self.data_criacao: str = ""
+        self.responsavel: str = ""
+        self.unidade_origem: str = ""
+        self.conteudo: str = ""
+
+
+class OdinParserData:
+    def __init__(self) -> None:
+        self.pericia = OdinParserDataPericia()
+        self.ocorrencia: str = ""
+        self.data_ocorrencia: str = ""
+        self.rai: str = ""
+        self.unidade_solicitante: str = ""
+        self.autoridade: str = ""
+        self.quesito = OdinParserDataQusito()
+        self.pessoas: list[str] = []
